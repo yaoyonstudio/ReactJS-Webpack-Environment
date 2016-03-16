90cab3761f0b12ae853af6149c99dacbaf89f3af
yaoyonstudio <yaozekun_999@163.com>
2016-03-16 16:02:42 +0800

    'init'

---
+++ src/js/components/Hello.js
@@ -0,0 +1,10 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+export default class Hello extends React.Component {
+  render(){
+    return (
+      <h1>Hello,{this.props.name}</h1>
+    );
+  }
+}
---
+++ src/js/main.js
@@ -0,0 +1,8 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+import Hello from './components/Hello';
+
+const app = document.getElementById('app');
+
+ReactDOM.render(<Hello name="kenny" />, app);
